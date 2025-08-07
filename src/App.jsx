2f974fef9823f1522fc78567c8fce007b9429b99
neubaeza <davidbaezaospino@gmail.com>
2025-08-07 11:00:26 -0500

    Subida inicial del frontend

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function App() {
+  return (
+    <div className="p-6 text-center text-xl font-bold text-gray-700">
+      Plataforma de Validación de Incapacidades
+    </div>
+  );
+}
